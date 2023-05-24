8845a3e12616462ecc20a126eea611659a16a77d
Mirtekore <engjiaee@gmail.com>
2023-05-24 13:17:24 +1000

    Changed files for demo

--- Mastodon/run.sh
+++ Mastodon/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# ansible-playbook -i hosts -u ubuntu --ask-vault-pass mastodon.yaml -vvv
-ansible-playbook -i hosts -u ubuntu mastodon.yaml -vvv
+ansible-playbook -i hosts -u ubuntu --ask-vault-pass mastodon.yaml -vvv
